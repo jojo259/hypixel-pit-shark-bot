e55db76f125c962958608cdf3a662059a297e04c
jojo259 <95320642+jojo259@users.noreply.github.com>
2023-01-06 05:41:05 +0000

    removed a log message

--- run.py
+++ run.py
@@ -17,8 +17,6 @@ while True:
 			indexedTimes.append(curTime)
 			playerToCheck = playermanager.getQueuedPlayer()
 			print(f'checking {playerToCheck}, checked {len(indexedTimes)} in last minute')
-			if random.randint(1, 120) == 1:
-				discordsender.sendDiscord(f'checking {playerToCheck}, checked {len(indexedTimes)} in last minute', config.discordWebhookUrl)
 			indexer.indexPlayer(playerToCheck)
 		else:
 			time.sleep(1)
