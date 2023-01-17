2fb3e27148e292c258482b81c3fe3bc4ad009369
jojo259 <95320642+jojo259@users.noreply.github.com>
2023-01-17 16:12:55 +0000

    Update checksharkable.py

--- checksharkable.py
+++ checksharkable.py
@@ -11,6 +11,7 @@ desiredEnchantsSwords = [
 	'melee_lightning',
 	'melee_gamble',
 	'melee_literally_p2w',
+	'gold_strictly_kills',
 ]
 
 desiredEnchantsSwordsTokensTwo = [
@@ -25,10 +26,28 @@ desiredEnchantsPants = [
 desiredEnchantsBows = [
 	'bow_slow',
 	'telebow',
+	'instant_shot',
+	'volley',
+	'bow_combo_speed',
+	'bow_weakness_on_hit',
+	'punch_once_in_a_while',
+	'pin_down',
 ]
 
 desiredEnchantCombinations = [
 	['melee_heal_on_hit', 'melee_damage_when_low']
+]
+
+desiredEnchantsTier3 = [
+	'melee_lightning',
+	'melee_execute',
+	'melee_heal_on_hit',
+	'melee_literally_p2w',
+	'instant_shot',
+]
+
+desiredEnchantCombinationsTier3 = [
+	['bow_slow', 'instant_shot']
 ]
 
 def getItemMatchedSharkableFilters(curItem):
@@ -40,8 +59,8 @@ def getItemMatchedSharkableFilters(curItem):
 
 	itemTier = util.getVal(curItem, ['tag','ExtraAttributes','UpgradeTier'])
 
-	if itemTier != 2:
-		return []
+	#if itemTier != 2:
+	#	return []
 
 	itemNonce = util.getVal(curItem, ['tag','ExtraAttributes','Nonce'])
 
@@ -69,23 +88,34 @@ def getItemMatchedSharkableFilters(curItem):
 		enchantLevel = curEnchant.get('Level')
 
 		desiredFilters = {
-			'any': enchantKey in desiredEnchantsAny,
-			'bowAny': itemId == 269 and enchantKey in desiredEnchantsBows,
-			'swordAny': itemId == 283 and enchantKey in desiredEnchantsSwords,
-			'pantsAny': itemId == 300 and enchantKey in desiredEnchantsPants,
-			'swordsTokensTwoPlus': itemId == 283 and enchantKey in desiredEnchantsSwordsTokensTwo and enchantLevel >= 2,
+			'any': itemTier == 2 and enchantKey in desiredEnchantsAny,
+			'bowAny': itemTier == 2 and itemId == 269 and enchantKey in desiredEnchantsBows,
+			'swordAny': itemTier == 2 and itemId == 283 and enchantKey in desiredEnchantsSwords,
+			'pantsAny': itemTier == 2 and itemId == 300 and enchantKey in desiredEnchantsPants,
+			'swordsTokensTwoPlus': itemTier == 2 and itemId == 283 and enchantKey in desiredEnchantsSwordsTokensTwo and enchantLevel >= 2,
+			'tier3testany': itemTier == 3 and enchantKey in desiredEnchantsTier3,
 		}
 
 		for filterName, filterMatch in desiredFilters.items():
 			if filterMatch == True:
 				matchedFilters.append(filterName)
 
-	for enchantCombination in desiredEnchantCombinations:
-		allFound = True
-		for curEnchant in enchantCombination:
-			if curEnchant not in itemPitEnchantsKeys:
-				allFound = False
-		if allFound:
-			matchedFilters.append('combination')
+	if itemTier == 2:
+		for enchantCombination in desiredEnchantCombinations:
+			allFound = True
+			for curEnchant in enchantCombination:
+				if curEnchant not in itemPitEnchantsKeys:
+					allFound = False
+			if allFound:
+				matchedFilters.append('combination')
+
+	if itemTier == 3:
+		for enchantCombination in desiredEnchantCombinationsTier3:
+			allFound = True
+			for curEnchant in enchantCombination:
+				if curEnchant not in itemPitEnchantsKeys:
+					allFound = False
+			if allFound:
+				matchedFilters.append('combinationTier3')
 
 	return matchedFilters
